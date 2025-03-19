8c84ea96ad80ad31caf787cfded2cd64aee822bc
Pradeep Kumar <122957394+Pradeepp15012@users.noreply.github.com>
2025-03-19 17:50:13 +0000

    weatherserver

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { z } from "zod";
 
 const server = new McpServer({
-    name: "Weather Data Fetcher",
-    version: '1.0.0',
-    
-})
+  name: "Weather Data Fetcher",
+  version: "1.0.0",
+});
+
+async function getWeatherByCity(city = "") {
+  if (city.toLowerCase() === "patiala") {
+    return { temp: "30C", forecast: "chances of high rain" };
+  }
+  if (city.toLowerCase() === "delhi") {
+    return { temp: "40C", forecast: "chances of high warm winds" };
+  }
+  return { temp: null, error: "Unable to get data" };
+}
+server.tool(
+  "getWeatherDataByCityName",
+  {
+    city: z.string(),
+  },
+  async ({ city }) => {
+    return {
+      content: [
+        { type: "text", text: JSON.stringify(await getWeatherByCity(city)) },
+      ],
+    };
+  }
+);
